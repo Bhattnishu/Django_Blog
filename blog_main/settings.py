09a21dba4ee0a90ad3ca601ea38612975589d8c2
Nishant Bhatt <nishantbhatt2017@gmail.com>
2026-08-29 11:34:05 +0530

    Configure static files for deployment

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -125,11 +125,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
